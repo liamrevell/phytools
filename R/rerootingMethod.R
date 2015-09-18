ebc4878f7e40e139616681f8667ecf5311cd9466
liamrevell <liamjrevell@gmail.com>
2015-09-18 10:40:37 -0400

    update to rerootingMethod to permit polytomies natively

--- R/rerootingMethod.R
+++ R/rerootingMethod.R
@@ -1,17 +1,13 @@
-# function to compute the marginal posterior probabilities for nodes using the rerooting method
-# written by Liam J. Revell 2013, 2015
+## function to compute the marginal posterior probabilities for nodes using the rerooting method
+## written by Liam J. Revell 2013, 2015
 
 rerootingMethod<-function(tree,x,model=c("ER","SYM"),...){
-	if(!inherits(tree,"phylo")) stop("tree should be an object of class \"phylo\".")
-	if(!is.binary.tree(tree)){
-		mt<-tree
-		tree<-multi2di(tree)
-		multif<-TRUE
-	} else multif<-FALSE
+	if(!inherits(tree,"phylo")) 
+		stop("tree should be an object of class \"phylo\".")
 	if(hasArg(tips)) tips<-list(...)$tips
 	else tips<-NULL
 	if(!is.matrix(model)) model<-model[1]
-	n<-length(tree$tip.label)
+	n<-Ntip(tree)
 	# if vector convert to binary matrix
 	if(!is.matrix(x)){ 
 		yy<-to.matrix(x,sort(unique(x)))
@@ -23,12 +19,12 @@ rerootingMethod<-function(tree,x,model=c("ER","SYM"),...){
 	yy<-yy[tree$tip.label,]
 	yy<-yy/rowSums(yy)
 	YY<-fitMk(tree,yy,model=model,output.liks=TRUE)
-	Q<-matrix(c(0,YY$rates)[YY$index.matrix+1],length(YY$states),length(YY$states),
-		dimnames=list(YY$states,YY$states))
+	Q<-matrix(c(0,YY$rates)[YY$index.matrix+1],length(YY$states),
+		length(YY$states),dimnames=list(YY$states,YY$states))
 	diag(Q)<--colSums(Q,na.rm=TRUE)
 	nn<-if(tips) c(1:n,2:tree$Nnode+n) else 2:tree$Nnode+n
 	ff<-function(nn){
-		tt<-reroot(tree,node.number=nn,position=tree$edge.length[which(tree$edge[,2]==nn)])
+		tt<-root(tree,node=nn)
 		fitMk(tt,yy,model=model,fixedQ=Q,output.liks=TRUE)$lik.anc[1,]
 	}
 	XX<-t(sapply(nn,ff))
@@ -37,11 +33,5 @@ rerootingMethod<-function(tree,x,model=c("ER","SYM"),...){
 	rownames(XX)<-1:(tree$Nnode+n)
 	if(tips) rownames(XX)[1:n]<-tree$tip.label
 	XX<-if(tips) XX else XX[1:tree$Nnode+n,]
-	if(multif){
-		M<-matchNodes(mt,tree)
-		ii<-sapply(M[,2],function(x,y) which(y==x),y=as.numeric(rownames(XX)))
-		XX<-XX[ii,]
-		rownames(XX)<-M[,1]		
-	}
 	return(list(loglik=YY$logLik,Q=Q,marginal.anc=XX))
 }
