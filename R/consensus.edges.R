d8b921a00e24120d64ed608fd19a6faf2a852aa1
Liam J. Revell <liam.revell@umb.edu>
2016-03-09 15:45:30 -0400

    function to compute consensus edges

---
+++ R/consensus.edges.R
@@ -0,0 +1,31 @@
+## compute consensus edge lengths from a set of trees given (or not) a consensus topology
+## written by Liam J. Revell 2016
+
+consensus.edges<-function(trees,method=c("mean.edge","least.squares"),...){
+	if(hasArg(consensus.tree)) tree<-list(...)$consensus.tree
+	else tree<-consensus(trees,p=0.5)
+	if(hasArg(if.absent)) if.absent<-list(...)$if.absent
+	else if.absent<-"zero"
+	N<-length(trees)
+	if(method[1]=="mean.edge"){
+		M<-lapply(trees,function(x,y) rbind(matchLabels(y,x),matchNodes(y,x)),y=tree)
+		nodes<-M[[1]][,1]
+		edge.length<-vector(mode="numeric",length=length(nodes))
+		for(i in 2:length(nodes)){
+			ii<-which(tree$edge[,2]==nodes[i])
+			n.absent<-0
+			for(j in 1:N){
+				edge.length[ii]<-edge.length[ii] +
+					if(!is.na(M[[j]][i,2])) trees[[j]]$edge.length[which(trees[[j]]$edge[,2]==M[[j]][i,2])]/N
+					else 0
+				if(is.na(M[[j]][i,2])) n.absent<-n.absent+1
+			}
+			if(if.absent=="ignore") edge.length[ii]<-edge.length[ii]*N/(N-n.absent)
+		}
+		tree$edge.length<-edge.length
+	} else if(method[1]=="least.squares"){
+		D<-Reduce('+',lapply(trees,function(x,t) cophenetic(x)[t,t],t=tree$tip.label))/N
+		tree<-nnls.tree(D,tree=tree,rooted=is.rooted(tree))
+	}
+	tree
+}
