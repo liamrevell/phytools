c267edce7612f1cbaba4638e2e65e9c370d8d8b1
Liam J. Revell <liam.revell@umb.edu>
2016-12-29 11:52:05 -0500

    S3 methods for phyl.RMA

--- R/phyl.RMA.R
+++ R/phyl.RMA.R
@@ -2,13 +2,15 @@
 ## written by Liam Revell 2010, 2011, 2012, 2015, 2016
 
 phyl.RMA<-function(x,y,tree,method="BM",lambda=NULL,fixed=FALSE,h0=1.0){
-	if(!inherits(tree,"phylo")) stop("tree should be an object of class \"phylo\".")
+	if(!inherits(tree,"phylo")) 
+		stop("tree should be an object of class \"phylo\".")
 	x<-x[tree$tip.label]; y<-y[tree$tip.label]
 	# bind the x & y into columns
 	X<-cbind(x,y)
 	if(method=="lambda")
 		if(fixed==FALSE)
-			result<-optimize(f=likMlambda,interval=c(0,1),X=X,C=vcv(tree),maximum=TRUE)
+			result<-optimize(f=likMlambda,interval=c(0,1),X=X,C=vcv(tree),
+				maximum=TRUE)
 		else
 			result<-list(objective=likMlambda(lambda,X,vcv(tree)),maximum=lambda)
 	else if(method=="BM")
@@ -31,5 +33,36 @@ phyl.RMA<-function(x,y,tree,method="BM",lambda=NULL,fixed=FALSE,h0=1.0){
 	df<-2+(Ntip(tree)-2)/(1+0.5*r2)
 	P<-2*pt(T,df=df,lower.tail=FALSE)
 	test<-c(r2,T,df,P); names(test)<-c("r2","T","df","P")
-	return(list(RMA.beta=c(beta0,beta1),V=temp$R,lambda=est.lambda,logL=as.numeric(result$objective),test=test,resid=as.matrix(r)))
+	object<-list(RMA.beta=c(beta0,beta1),V=temp$R,lambda=est.lambda,
+		logL=as.numeric(result$objective),test=test,h0=h0,
+		resid=as.matrix(r))
+	class(object)<-"phyl.RMA"
+	object
 }
+
+print.phyl.RMA<-function(x,...){
+	cat("\nCoefficients:\n")
+	print(coef(x))
+	cat("\nVCV matrix:\n")
+	print(x$V)
+	cat("\n")
+	if(is.null(x$lambda))
+		cat("Model for the covariance structure of the error is \"BM\"\n\n")
+	else {
+		cat("Model for the covariance structure of the error is \"lambda\"\n")
+		cat("\nEstimate(s):\n")
+		print(setNames(c(x$lambda,x$logL),c("lambda","log(L)")))
+		cat("\n")
+	}
+	cat("Hypothesis test based on Clarke (1980; Biometrika):\n")
+	print(x$test)
+	cat(paste("\nNote that the null hypothesis test is h0 =",x$h0,
+		"\n\n"))
+}
+
+coef.phyl.RMA<-function(object,...){
+	val<-setNames(object$RMA.beta,c("(Intercept)","x"))
+	val
+}
+
+residuals.phyl.RMA<-function(object,...) object$resid[,1]
