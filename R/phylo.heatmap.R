d2f1e1fd0d0ccb8290096b2a4771c38dc92a658b
Liam J. Revell <liam.revell@umb.edu>
2017-05-08 14:19:00 -0500

    fix to phylo.heatmap

--- R/phylo.heatmap.R
+++ R/phylo.heatmap.R
@@ -1,5 +1,5 @@
 ## function for phylogenetic heat map
-## written by Liam J. Revell 2016
+## written by Liam J. Revell 2016, 2017
 
 phylo.heatmap<-function(tree,X,fsize=1,colors=NULL,standardize=FALSE,...){
 	if(length(fsize)!=3) fsize<-rep(fsize,3)
@@ -24,26 +24,27 @@ phylo.heatmap<-function(tree,X,fsize=1,colors=NULL,standardize=FALSE,...){
 	if(hasArg(mar)) mar<-list(...)$mar
 	else mar<-rep(1.1,4)
 	if(is.null(colors)) colors<-heat.colors(n=20)[20:1]
-	cw<-reorder(tree,"cladewise")
+	cw<-untangle(tree,"read.tree")
 	plot.new()
 	par(mar=mar)
 	plot.window(xlim=xlim,ylim=ylim)
-	h<-phylogram(tree,fsize=fsize[1],...)
+	h<-phylogram(cw,fsize=fsize[1],...)
 	START<-h+1/2*((2-0.5)*split[2]/split[1]+0.5-h)/(ncol(X)-1)+
 		0.5*strwidth("W")*fsize[1]
 	END<-(2-0.5)*split[2]/split[1]+0.5-1/2*((2-0.5)*split[2]/split[1]+
-		0.5-START)/(ncol(X)-1)
+		0.5-START)/(ncol(X)-1
+	X<-X[cw$tip.label,]
 	image(x=seq(START,END,by=(END-START)/(ncol(X)-1)),
 		z=t(X[cw$tip.label,]),add=TRUE,
 		col=colors,...)
 	if(legend) add.color.bar(leg=END-START,cols=colors,lims=range(X,na.rm=TRUE),
 		title=if(standardize) "standardized value" else "value",
 		subtitle=if(standardize) "SD units" else "",prompt=FALSE,x=START,
-		y=-1/(2*(Ntip(tree)-1))-3*fsize[3]*strheight("W"),
+		y=-1/(2*(Ntip(cw)-1))-3*fsize[3]*strheight("W"),
 		digits=if(max(abs(X),na.rm=TRUE)<1) round(log10(1/max(abs(X),na.rm=TRUE)))+1 
 		else 2,fsize=fsize[3])
 	if(labels) text(x=seq(START,END,by=(END-START)/(ncol(X)-1)),
-		y=rep(1+1/(2*(Ntip(tree)-1))+0.4*fsize[2]*strwidth("I"),ncol(X)),
+		y=rep(1+1/(2*(Ntip(cw)-1))+0.4*fsize[2]*strwidth("I"),ncol(X)),
 		colnames(X),srt=70,adj=c(0,0.5),cex=fsize[2])
 	if(any(is.na(X))){
 		ii<-which(is.na(X),arr.ind=TRUE)
