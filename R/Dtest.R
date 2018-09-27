8bc9df23febae17ee408bb381790cb764a39acee
Liam J. Revell <liam.revell@umb.edu>
2018-09-27 14:20:48 -0300

    add Dtest to namespace

--- R/Dtest.R
+++ R/Dtest.R
@@ -1,3 +1,6 @@
+## function to conduct the correlation test of Huelsenbeck et al.
+## written by Liam J. Revell
+
 calcD<-function(t1,t2){
 	Do<-mapply(Map.Overlap,t1,t2,SIMPLIFY=FALSE)
 	foo<-function(M){
@@ -14,6 +17,9 @@ calcD<-function(t1,t2){
 }
 
 Dtest<-function(t1,t2,nsim=100,...){
+	cat("Note that this function is provided without much testing.\n")
+	cat("Please use with caution.\n\n")
+	cat("Running. (This may take some time.)\n")
 	levs1<-sort(unique(as.vector(getStates(t1,"tips"))))
 	k1<-length(levs1)
 	levs2<-sort(unique(as.vector(getStates(t2,"tips"))))
@@ -29,15 +35,27 @@ Dtest<-function(t1,t2,nsim=100,...){
 	for(i in 1:nrep){
 		x<-to.matrix(sim.Mk(t1[[i]],t1[[i]]$Q),levs1)
 		y<-to.matrix(sim.Mk(t2[[i]],t2[[i]]$Q),levs2)
-		T1<-make.simmap(t1[[i]],x,nsim=nsim,message=FALSE)
-		T2<-make.simmap(t2[[i]],y,nsim=nsim,message=FALSE)
+		T1<-make.simmap(t1[[i]],x,nsim=nsim,message=FALSE,...)
+		T2<-make.simmap(t2[[i]],y,nsim=nsim,message=FALSE,...)
 		tmp<-calcD(T1,T2)
 		PD<-PD+(tmp$E_DX>=E_DX)/nrep
 		Pdij<-Pdij+(tmp$E_Dij>=E_Dij)/nrep
 		cat(".")
-		if(i%%10==0) cat("\n")
+		if(i%%10==0&&i!=nrep) cat("\n")
 		dev.flush()
 	}
-	list("E(D|X)"=E_DX,"P(D)"=PD,"E(Dij)"=E_Dij,
+	cat("|\nDone.\n")
+	obj<-list("E(D|X)"=E_DX,"P(D)"=PD,"E(Dij)"=E_Dij,
 		"P(Dij)"=Pdij)
+	class(obj)<-"Dtest"
+	obj
+}
+
+print.Dtest<-function(x,...){
+	if(hasArg(digits)) digits<-list(...)$digits
+    else digits<-4
+    x<-lapply(x,function(a,b) if(is.numeric(a)) round(a,b) else a,b=digits)
+	cat("Summary of results from D-test:\n")
+	cat(paste(" E(D|X) = ",x$'E(D|X)',", P(D) = ",x$'P(D)',"\n",sep=""))
+	cat("\n(Type ...$'E(Dij)' and ...$'P(Dij)' for\n pairwise E(D) and P-values.)\n")
 }
