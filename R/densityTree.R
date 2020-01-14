5942caa057f82358704261cc89e7fede646483dc
Liam J. Revell <liam.revell@umb.edu>
2020-01-14 16:40:17 -0300

    vectorization of make.transparent

--- R/densityTree.R
+++ R/densityTree.R
@@ -1,7 +1,22 @@
 ## function to make a color (e.g., "blue") transparent with alpha level alpha
 make.transparent<-function(color,alpha){
-	RGB<-col2rgb(color)[,1]/255
-	rgb(RGB[1],RGB[2],RGB[3],alpha)
+	if(length(color)>1 && length(alpha)>1){
+		if(length(color) != length(alpha)){
+			cat("Lengths of color and alpha should match.\n")
+			cat("Using only first alpha value....\n")
+			cols<-make.transparent(color,alpha[1])
+		} else {
+			cols<-mapply(make.transparent,color=color,alpha=alpha)
+		}
+	} else if(length(color)>1 && length(alpha)==1){
+		cols<-sapply(color,make.transparent,alpha=alpha)
+	} else if(length(color)==1 && length(alpha)>1){
+		cols<-sapply(alpha,make.transparent,color=color)
+	} else {
+		RGB<-col2rgb(color)[,1]/255
+		cols<-rgb(RGB[1],RGB[2],RGB[3],alpha)
+	}
+	cols
 }
 
 ## function to be used internally
