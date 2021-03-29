2f4bdd14b9243b130d96a544089fa12dc83ad272
Liam J. Revell <liamjrevell@gmail.com>
2021-03-28 22:41:35 -0400

    updates and fixes

--- R/multirateBM.R
+++ R/multirateBM.R
@@ -99,22 +99,30 @@ logLik.multirateBM<-function(x){
 }
 
 plot.multirateBM<-function(x,digits=1,...){
-	plot.phylo(x$tree,no.margin=TRUE,plot=FALSE,...)
+	cols<-setNames(rainbow(1000,start=0.7,end=0),
+		1:1000)
+	est.sig2<-apply(x$tree$edge,1,function(e,x) 
+		mean(x[e]),x=x$sig2)
+	ln.sig2<-log(est.sig2)
+	min.sig2<-min(ln.sig2)
+	max.sig2<-max(ln.sig2)
+	edge.states<-vector()
+	for(i in 1:length(est.sig2)){
+		edge.states[i]<-round((ln.sig2[i]-min.sig2)/
+			(max.sig2-min.sig2)*999)+1
+	}
+	tree<-paintBranches(x$tree,edge=x$tree$edge[1,2],
+		state=edge.states[1])
+	for(i in 2:length(edge.states))
+		tree<-paintBranches(tree,edge=tree$edge[i,2],
+			state=edge.states[i])
+	plotTree(x$tree,plot=FALSE,...)
 	lastPP<-get("last_plot.phylo",envir=.PlotPhyloEnv)
-	obj<-plotBranchbyTrait(x$tree,log(x$sig2),
-		mode="nodes",
-		x.lim=c(-0.3,1)*diff(lastPP$x.lim),
-		legend=FALSE,...)
+	obj<-plot(tree,colors=cols,lwd=3,split.vertical=TRUE,
+		xlim=c(-0.3,1.05)*diff(lastPP$x.lim),
+		add=TRUE,...)
 	lastPP<-get("last_plot.phylo",envir=.PlotPhyloEnv)
 	h<-max(nodeHeights(x$tree))
-	add.color.bar(Ntip(x$tree)-3,
-		rainbow(1000,start=0.7,end=0),
-		title=expression(paste("evolutionary rate ( ",sigma^2,")")),
-		lims=NULL,digits=3,
-		direction="upwards",
-    		subtitle="",lwd=15,
-		x=-0.25*h,
-		y=2,prompt=FALSE)
 	LWD<-diff(par()$usr[1:2])/dev.size("px")[1]
 	lines(x=rep(-0.25*h+LWD*15/2,2),y=c(2,Ntip(x$tree)-1))
 	nticks<-10
@@ -125,6 +133,14 @@ plot.multirateBM<-function(x,digits=1,...){
 	for(i in 1:nrow(Y)) lines(X[i,],Y[i,])
 	ticks<-exp(seq(min(log(x$sig2)),max(log(x$sig2)),
 		length.out=nticks))
+	add.color.bar(Ntip(x$tree)-3,
+		rainbow(1000,start=0.7,end=0),
+		title=expression(paste("evolutionary rate ( ",sigma^2,")")),
+		lims=NULL,digits=3,
+		direction="upwards",
+    		subtitle="",lwd=15,
+		x=-0.25*h,
+		y=2,prompt=FALSE)
 	text(x=X[,2],y=Y[,2],signif(ticks,digits),pos=4,cex=0.7)
 }
 
