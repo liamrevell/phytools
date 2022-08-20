133915a88a1df6638c9cb5b25e3b7ac58f2ed75e
Liam J. Revell <liam.revell@umb.edu>
2022-08-20 13:14:04 -0400

    some fixes for fitpolyMk

--- R/fitpolyMk.R
+++ R/fitpolyMk.R
@@ -16,7 +16,8 @@ fitpolyMk<-function(tree,x,model="SYM",ordered=FALSE,...){
 	if(hasArg(quiet)) quiet<-list(...)$quiet
 	else quiet<-FALSE
 	if(is.factor(x)) x<-setNames(as.character(x),names(x))
-	X<-strsplit(x,"+",fixed=TRUE)
+	if(is.matrix(x)) X<-strsplit(colnames(x),"+",fixed=TRUE)
+	else X<-strsplit(x,"+",fixed=TRUE)
 	ns<-sapply(X,length)
 	if(ordered){
 		if(hasArg(max.poly)) max.poly<-list(...)$max.poly
@@ -30,7 +31,10 @@ fitpolyMk<-function(tree,x,model="SYM",ordered=FALSE,...){
 		object<-NULL
 	} else {
 		## fix the order of the input data
-		x<-sapply(X,function(x) paste(sort(x),collapse="+"))
+		if(is.matrix(x)){
+			Levs<-sapply(X,function(x) paste(sort(x),collapse="+"))
+			colnames(x)<-Levs
+		} else x<-sapply(X,function(x) paste(sort(x),collapse="+"))
 		## get the states
 		states<-sort(unique(unlist(X)))
 		if(ordered){
@@ -87,7 +91,10 @@ fitpolyMk<-function(tree,x,model="SYM",ordered=FALSE,...){
 			cat("\n")
 			flush.console()
 		}
-		X<-to.matrix(x,ss)
+		if(is.matrix(x)){
+			X<-matrix(0,nrow(x),length(ss),dimnames=list(rownames(x),ss))
+			X[rownames(x),colnames(x)]<-x
+		} else X<-to.matrix(x,ss)
 		object<-fitMk(tree,X,model=tmodel,...)
 	}
 	object$model<-model
