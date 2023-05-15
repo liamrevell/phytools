59c296b3fdd9adcac15169eb81dd80ba98e9aa46
Liam J. Revell <liamjrevell@gmail.com>
2023-05-14 21:06:38 -0400

    add joint reconstruction using Pupko 2000 algorithm

--- R/asr.R
+++ R/asr.R
@@ -1,8 +1,80 @@
 ## functions to do the pruning algorithm and reconstruct ancestral states
 
+joint_asr<-function(q,tree,X,pi,model=NULL,tips=FALSE,tol=1e-12){
+	cat("Note:\n")
+		cat("	 this option is new & I recommend cross-checking\n")
+		cat("	 your reconstruction with corHMM::ancRECON.\n")
+	tol<-tol*max(nodeHeights(tree))
+	tipn<-1:Ntip(tree)
+	tt<-tree
+	for(i in 1:length(tipn)) 
+		tt<-paintSubTree(tt,tipn[i],state="2",stem=tol)
+	tt<-map.to.singleton(tt)
+	pw<-reorder(tt,"postorder")
+	X<-X[pw$tip.label,]
+	k<-ncol(X)
+	if(is.null(model)){
+		model<-matrix(1,k,k)
+		diag(model)<-0
+	}
+	Q<-matrix(0,k,k)
+	Q[]<-c(0,q)[model+1]
+	diag(Q)<--rowSums(Q)
+	C<-matrix(NA,Nnode(pw)+Ntip(pw),ncol(X),
+		dimnames=list(1:(Ntip(pw)+Nnode(pw))))
+	colnames(C)<-colnames(X)
+	L<-matrix(0,nrow(C),k,dimnames=dimnames(C))
+	for(i in 1:Ntip(pw)){
+		ind<-which(pw$edge[,2]==i)
+		P<-expm(Q*pw$edge.length[ind])
+		L[i,]<-log(X[i,]%*%P)
+	}
+	nn<-unique(pw$edge[,1])
+	chk<-FALSE
+	for(i in 1:(length(nn)-1)){
+		P<-expm(Q*pw$edge.length[which(pw$edge[,2]==nn[i])])
+		daughters<-pw$edge[which(pw$edge[,1]==nn[i]),2]
+		for(j in 1:k){
+			pp<-log(P[j,])+colSums(L[daughters,,drop=FALSE])
+			L[nn[i],j]<-max(pp)
+			if(length(which(pp==max(pp)))>1) chk<-TRUE
+			C[nn[i],j]<-sample(x=names(pp)[which(pp==max(pp))],size=1)
+		}
+	}
+	if(chk){
+		cat("Note:\n")
+		cat("	 there may be more than one equally likely\n")
+		cat("	 joint reconstruction.\n")
+	}
+	ROOT<-Ntip(pw)+1
+	daughters<-pw$edge[which(pw$edge[,1]==ROOT),2]
+	L[ROOT,]<-log(pi)+colSums(L[daughters,])
+	C[ROOT,]<-names(which(L[ROOT,]==max(L[ROOT,])))
+	cw<-reorder(pw)
+	nn<-unique(as.vector(cw$edge))
+	states<-setNames(rep(NA,length(nn)),nn)
+	states[1]<-C[ROOT,1]
+	logL<-max(L[ROOT,])
+	for(i in 2:length(nn)){
+		mother<-cw$edge[which(cw$edge[,2]==nn[i]),1]
+		states[i]<-C[nn[i],states[as.character(mother)]]
+	}
+	M<-matchNodes(tree,tt,method="distances")
+	anc<-setNames(states[as.character(M[,2])],M[,1])
+	if(tips){
+		pp<-sapply(1:Ntip(tt),getParent,tree=tt)
+		anc<-c(setNames(states[as.character(pp)],tt$tip.label[1:Ntip(tt)]),
+			anc)
+	}
+	attr(logL,"df")<-length(anc)
+	object<-list(ace=as.factor(anc),logLik=logL)
+	object
+}
+
 ## plot method for "ancr" object class
 plot.ancr<-function(x,args.plotTree=list(...),args.nodelabels=list(...),...){
 	TREE<-attr(x,"tree")
+	TYPE<-attr(x,"type")
 	args.plotTree$tree<-TREE
 	if(is.null(args.plotTree$type))
 		args.plotTree$type<-"phylogram"
@@ -26,16 +98,27 @@ plot.ancr<-function(x,args.plotTree=list(...),args.nodelabels=list(...),...){
 			if(args.plotTree$direction%in%c("rightwards","leftwards"))
 				args.plotTree$offset<-0.4
 			else args.plotTree$offset<-1
-		}  else args.plotTree$offset<-2
+		} else args.plotTree$offset<-2
 	}
 	do.call(plotTree,args.plotTree)
-	if(nrow(x$ace)==Nnode(TREE)){
-		args.nodelabels$pie<-x$ace
-		data<-attr(x,"data")
-	} else if(nrow(x$ace)==(Ntip(TREE)+Nnode(TREE))){
-		args.nodelabels$pie<-x$ace[1:Nnode(TREE)+Ntip(TREE),,drop=FALSE]
-		data<-x$ace[1:Ntip(TREE),,drop=FALSE]
-	} else cat("Warning: wrong number of rows in input object.\n\n")
+	if(TYPE=="marginal"){
+		if(nrow(x$ace)==Nnode(TREE)){
+			args.nodelabels$pie<-x$ace
+			data<-attr(x,"data")
+		} else if(nrow(x$ace)==(Ntip(TREE)+Nnode(TREE))){
+			args.nodelabels$pie<-x$ace[1:Nnode(TREE)+Ntip(TREE),,drop=FALSE]
+			data<-x$ace[1:Ntip(TREE),,drop=FALSE]
+		} else cat("Warning: wrong number of rows in input object.\n\n")
+	} else if(TYPE=="joint"){
+		if(length(x$ace)==Nnode(TREE)){
+			data<-attr(x,"data")
+			args.nodelabels$pie<-to.matrix(x$ace,colnames(data))
+		} else if(nrow(x$ace)==(Ntip(TREE)+Nnode(TREE))){
+			args.nodelabels$pie<-to.matrix(x$ace[1:Nnode(TREE)+Ntip(TREE)],
+				colnames(attr(x,"data")))
+			data<-to.matrix(x$ace[1:Ntip(TREE)],colnames(attr(x,"data")))
+		} else cat("Warning: wrong number of elements in input object.\n\n")
+	}
 	if(is.null(args.nodelabels$cex)){
 		if(args.plotTree$type%in%c("phylogram","cladogram")){
 			if(args.plotTree$direction%in%c("rightwards","leftwards"))
@@ -46,7 +129,7 @@ plot.ancr<-function(x,args.plotTree=list(...),args.nodelabels=list(...),...){
 			args.nodelabels$cex<-min(c(0.5*min(par()$pin)/sqrt(Ntip(TREE)),0.5))
 		}
 	}
-	k<-ncol(x$ace)
+	k<-if(TYPE=="marginal") ncol(x$ace) else ncol(attr(x,"data"))
 	if(is.null(args.nodelabels$piecol)){
 		args.nodelabels$piecol<-palette.colors(k,"Polychrome 36",recycle=TRUE)
 		if(k>36) 
@@ -73,7 +156,7 @@ plot.ancr<-function(x,args.plotTree=list(...),args.nodelabels=list(...),...){
 	if(hasArg(legend)) legend<-list(...)$legend
 	else legend<-"bottomleft"
 	if(legend!=FALSE){
-		legend(x=legend,legend=colnames(x$ace),
+		legend(x=legend,legend=colnames(attr(x,"data")),
 			pch=16,col=args.nodelabels$piecol,
 			pt.cex=1.2,cex=0.8)
 	}
@@ -101,12 +184,22 @@ logLik.ancr<-function(object,...){
 }
 
 print.ancr<-function(x,digits=6,printlen=6,...){
-	cat("Marginal ancestral state estimates:\n")
-	if (is.null(printlen)) 
-		print(round(x$ace,digits))
-	else {
-		print(round(x$ace[1:printlen,],digits))
-		cat("...\n")
+	if(attr(x,"type")=="marginal"){
+		cat("Marginal ancestral state estimates:\n")
+		if (is.null(printlen)) 
+			print(round(x$ace,digits))
+		else {
+			print(round(x$ace[1:printlen,],digits))
+			cat("...\n")
+		}
+	} else if(attr(x,"type")=="joint"){
+		cat("Joint ancestral state estimates:\n")
+		tmp<-data.frame(state=x$ace)
+		if(is.null(printlen)) print(tmp)
+		else {
+			print(tmp[1:printlen,,drop=FALSE])
+			cat("...\n")
+		}
 	}
 	cat(paste("\nLog-likelihood =",round(logLik(x),
 		digits),"\n\n"))
@@ -203,23 +296,36 @@ ancr.fitpolyMk<-function(object,...) ancr.fitMk(object,...)
 
 ## marginal ancestral states for "fitMk" object
 ancr.fitMk<-function(object,...){
+	if(hasArg(type)) type<-list(...)$type
+	else type<-"marginal"
+	if(hasArg(tips)) tips<-list(...)$tips
+	else tips<-FALSE
 	x<-object$data
 	tree<-object$tree
 	q<-object$rates
 	model<-object$index.matrix
 	model[is.na(model)]<-0
 	pi<-object$pi
-	plik<-pruning(q,tree,x,model=model,pi=pi,
-		return="conditional")
-	if(hasArg(tips)) tips<-list(...)$tips
-	else tips<-FALSE
-	ace<-marginal_asr(q,tree,plik,model,tips)
-	result<-list(ace=ace,
-		logLik=pruning(q,tree,x,model=model,pi=pi))
-	attr(result$logLik,"df")<-max(model)
-	attr(result,"tree")<-tree
-	attr(result,"data")<-x
-	class(result)<-"ancr"
+	if(type=="marginal"){
+		plik<-pruning(q,tree,x,model=model,pi=pi,
+			return="conditional")
+		ace<-marginal_asr(q,tree,plik,model,tips)
+		result<-list(ace=ace,
+			logLik=pruning(q,tree,x,model=model,pi=pi))
+		attr(result$logLik,"df")<-max(model)
+		attr(result,"type")<-"marginal"
+		attr(result,"tree")<-tree
+		attr(result,"data")<-x
+		class(result)<-"ancr"
+	} else if(type=="joint"){
+		if(hasArg(tol)) tol<-list(...)$tol
+		else tol<-1e-12
+		result<-joint_asr(q,tree,x,pi,model,tips,tol)
+		attr(result,"type")<-"joint"
+		attr(result,"tree")<-tree
+		attr(result,"data")<-x
+		class(result)<-"ancr"
+	}
 	result
 }
 
