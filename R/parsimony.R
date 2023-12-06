75fa12611891af8e3a4e573a6b27744301bc17de
Liam J. Revell <liamjrevell@gmail.com>
2023-12-05 21:56:55 -0500

    acctran & deltran (not finished)

--- R/parsimony.R
+++ R/parsimony.R
@@ -30,3 +30,41 @@ pscore<-function(tree,x,...){
 		Fitch(x,pw,nn,nm,...)
 	}
 }
+
+acctran<-function(tree,x,...){
+	pw<-if(!is.null(attr(tree,"order"))&&
+			attr(tree,"order")=="postorder") tree else 
+		reorder(tree,"postorder")
+	cw<-if(!is.null(attr(tree,"order"))&&
+			attr(tree,"order")=="cladewise") tree else
+		reorder(tree,"cladewise")
+	nn<-unique(pw$edge[,1])
+	if(is.matrix(x)||is.data.frame(x)){
+		nm<-rownames(x)
+		Nodes<-apply(x,2,AccDelTran,pw=pw,cw=cw,nn=nn,nm=nm,
+			method="acctran")
+	} else {
+		nm<-names(x)
+		AccDelTran(x,pw,cw,nn,nm,method="acctran")
+	}
+}
+
+AccDelTran<-function(x,pw,cw,nn,nm,method="acctran"){
+	Sets<-Fitch(x,pw,nn,nm,return="nodes")
+  ## preorder traversal
+	for(i in length(nn):1){
+	  ee<-which(cw$edge[,1]==nn[i])
+	  
+	}
+
+#	ee<-which(pw$edge[,1]==nn[i])
+#	Intersection<-Reduce(intersect,xx[pw$edge[ee,2]])
+#	if(length(Intersection)>0){ 
+#	  xx[[nn[i]]]<-Intersection
+#	} else {
+#	  xx[[nn[i]]]<-Reduce(union,xx[pw$edge[ee,2]])
+#	  pp<-pp+1
+#	}
+	
+	
+}
