ccbf31a764f568842bcd6bdbbab2ef5c784532c5
Liam J. Revell <liamjrevell@gmail.com>
2025-05-19 17:53:57 -0400

    add fitsemiThresh method

--- R/fitThresh.R
+++ R/fitThresh.R
@@ -42,36 +42,36 @@ t2bin_v1<-function(liability,threshold,X){
 }
 
 t2bin_v2<-function(liability,threshold,X){
-  Y<-matrix(0,length(liability),nrow(X),
-    dimnames=list(round(liability,3),rownames(X)))
-  interval<-mean(liability[2:length(liability)]-
-      liability[1:(length(liability)-1)])
-  down<-liability-interval/2
-  ## down[1]<--Inf
-  up<-liability+interval/2
-  ## up[length(up)]<-Inf
-  xx<-rep(0,length(liability))
-  for(i in 1:(length(threshold)-1)){
-    xx[]<-0
-    ind<-intersect(which(up>threshold[i]),
-      which(down<threshold[i+1]))
-    xx[ind]<-1
-    ind<-setdiff(ind,
-      intersect(which(down>threshold[i]),
-        which(up<threshold[i+1])))
-    if(length(ind)>0){
-      for(j in 1:length(ind)){
-        xx[ind[j]]<-if(down[ind[j]]>threshold[i]&&
-            down[ind[j]]<threshold[i+1]) 
-          (threshold[i+1]-down[ind[j]])/interval else
-            abs(up[ind[j]]-threshold[i])/interval
-      }
-    }
-    ii<-which(X[,i]>0)
-    Y[,ii]<-Y[,ii]+matrix(rep(xx,length(ii)),length(xx),
-      length(ii))*sapply(X[ii,i],rep,times=length(xx))
-  }
-  t(Y)
+	Y<-matrix(0,length(liability),nrow(X),
+		dimnames=list(round(liability,3),rownames(X)))
+	interval<-mean(liability[2:length(liability)]-
+			liability[1:(length(liability)-1)])
+	down<-liability-interval/2
+	## down[1]<--Inf
+	up<-liability+interval/2
+	## up[length(up)]<-Inf
+	xx<-rep(0,length(liability))
+	for(i in 1:(length(threshold)-1)){
+		xx[]<-0
+		ind<-intersect(which(up>threshold[i]),
+			which(down<threshold[i+1]))
+		xx[ind]<-1
+		ind<-setdiff(ind,
+			intersect(which(down>threshold[i]),
+				which(up<threshold[i+1])))
+		if(length(ind)>0){
+			for(j in 1:length(ind)){
+				xx[ind[j]]<-if(down[ind[j]]>threshold[i]&&
+						down[ind[j]]<threshold[i+1]) 
+					(threshold[i+1]-down[ind[j]])/interval else
+						abs(up[ind[j]]-threshold[i])/interval
+			}
+		}
+		ii<-which(X[,i]>0)
+		Y[,ii]<-Y[,ii]+matrix(rep(xx,length(ii)),length(xx),
+			length(ii))*sapply(X[ii,i],rep,times=length(xx))
+	}
+	t(Y)
 }
 
 ## fit threshold model using discrete approximation
@@ -246,4 +246,106 @@ lik_thresh<-function(threshold,pw,fixed_threshold,
 			return(-Inf) else return(prob)
 	else if(return=="conditional") L
 	else if(return=="pi") pi
+}
+
+fitsemiThresh<-function(tree,x,threshold=c(0,1),...){
+	if(hasArg(trace)) trace<-list(...)$trace
+	else trace<-0
+	if(hasArg(levs)) levs<-list(...)$levs
+	else levs<-200
+	if(hasArg(root)) root=list(...)$root
+	else root<-"mle"
+	if(root=="nuisance") pi<-"fitzjohn"
+	else if(root=="mle") pi<-"mle"
+	else if(root=="flat") pi<-rep(1/levs,levs)
+	if(hasArg(lik.func)) lik.func<-list(...)$lik.func
+	else lik.func<-"pruning"
+	if(hasArg(expm.method)) expm.method<-list(...)$expm.method
+	else expm.method<-"R_Eigen"
+	if(hasArg(p)) p<-list(...)$p
+	else p<-4
+	C<-vcv(tree)
+	N<-Ntip(tree)
+	## continuous character
+	x<-x[tree$tip.label]
+	if(is.null(threshold)){
+		threshold<-range(x)
+		LIMS<-expand.range(threshold,p=p)
+	} else if(threshold[1]==-Inf&&threshold[2]==Inf){
+		LIMS<-expand.range(range(x),p=p)
+		threshold<-LIMS
+	} else if(threshold[2]==Inf){
+		LIMS<-expand.range(c(threshold[1],max(x)),p=p)
+		threshold[2]<-LIMS[2]
+	} else if(threshold[1]==-Inf){
+		LIMS<-expand.range(c(min[x],threshold[2]),p=p)
+		threshold[1]<-LIMS[1]
+	} else {
+		LIMS<-expand.range(threshold,p=p)
+	}
+	dd<-diff(LIMS)
+	tol<-1e-8*dd/levs
+	n2<-round(diff(threshold)/diff(LIMS)*levs)+1
+	bb<-seq(threshold[1]+tol,threshold[2]-tol,length.out=n2)
+	w<-diff(range(bb))/(n2-1)
+	n1<-if(LIMS[1]<threshold[1]) 
+		round((threshold[1]-LIMS[1])/diff(LIMS)*levs) else 0
+	n3<-levs-n2-n1+1
+	bb<-c(seq(to=threshold[1]-w,by=w,length.out=n1),
+		bb,
+		seq(from=threshold[2]+w,by=w,length.out=n3))
+	bins<-cbind(bb[1:levs],bb[2:(levs+1)])
+	X<-to_binned(x,bins)
+	ll<-which(bins[,1]<=threshold[1])
+	uu<-which(bins[,2]>=threshold[2])
+	for(i in 1:length(x)){
+		if(x[i]<=threshold[1]) X[i,ll]<-1
+		else if(x[i]>=threshold[2]) X[i,uu]<-1
+	}
+	MODEL<-matrix(0,ncol(X),ncol(X),
+		dimnames=list(colnames(X),colnames(X)))
+	for(i in 1:(nrow(MODEL)-1)) MODEL[i,i+1]<-MODEL[i+1,i]<-1
+	pic_x<-pic(x,multi2di(tree))
+	nn<-max(c(floor(0.2*multi2di(tree)$Nnode),1))
+	q.init<-runif(n=1,0,2)*
+		(1/2)*mean(sort(pic_x^2,decreasing=TRUE)[1:nn])*
+		(levs/dd)^2
+	if(hasArg(max.sigsq)) max.sigsq<-list(...)$max.sigsq
+	else max.sigsq<-2*mean(sort(pic_x^2,decreasing=TRUE)[1:nn])
+	max.q=(1/2)*max.sigsq*(levs/dd)^2
+	mk_fit<-fitMk(tree,X,model=MODEL,pi=pi,lik.func=lik.func,
+		expm.method=expm.method,logscale=TRUE,max.q=max.q,
+		q.init=q.init)
+	lik<-logLik(mk_fit)-Ntip(tree)*log(dd/levs)
+	x0<-sum(mk_fit$pi*rowMeans(bins))
+	sigsq<-2*mk_fit$rates*(dd/levs)^2
+	object<-list(
+		sigsq=sigsq,
+		x0=x0,
+		threshold=threshold,
+		ncat=levs,
+		logLik=lik,
+		opt_results=mk_fit$opt_results,
+		at_bounds=(sigsq>=(max.sigsq-tol)),
+		mk_fit=mk_fit)
+	class(object)<-"fitsemiThresh"
+	object
+}
+
+print.fitsemiThresh<-function(x,digits=6,...){
+	cat(paste("Object of class \"fitsemiThresh\" based on\n",
+		"	a discretization with k =",
+		x$ncat,"levels.\n"))
+	cat(paste("Set or estimated thresholds: [",round(x$threshold[1],digits),
+		",",round(x$threshold[2],digits),"]\n\n"))
+	cat("Fitted model parameters:\n")
+	if(is.null(x$CI)) cat(paste("  sigsq:",round(x$sigsq,6),"\n"))
+	else cat(paste("  sigsq:",round(x$sigsq,6),"  [",round(x$CI[1],4),
+		",",round(x$CI[2],4),"]\n"))
+	cat(paste("     x0:",round(x$x0,6),"\n\n"))
+	cat(paste("Log-likelihood:",round(x$logLik,digits),"\n\n"))
+	if(x$opt_results$convergence == 0 && !x$at_bounds) 
+		cat("R thinks it has found the ML solution.\n\n")
+	else if(x$at_bounds) cat("Optimization may be at bounds.\n\n")
+	else cat("R thinks optimization may not have converged.\n\n")
 }
